c25c21136feb491012912f8070f016c3a9bc5e67
ori-git-acc <riocandido11@gmail.com>
2024-11-05 16:11:16 +0800

    db cleanup
    home page do not display after login when user is non admin

--- components/HomePage.js
+++ components/HomePage.js
@@ -60,7 +60,7 @@ const HomePage = () => {
 		>
 			<main className="flex flex-col gap-8 items-center sm:items-center bg-gray-900">
 				<h1 className="text-3xl font-bold text-center">Hi {username}, welcome to mini debt system!</h1>
-				<div className="flex gap-4 items-center flex-col sm:flex-row">
+				<div className="flex gap-4 items-center flex-row flex-wrap justify-center">
 					<button
 						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#b3b3b3] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
 						onClick={handleAddDebtClick}
