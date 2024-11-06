a490f839bec77c2f47521dc1a5742dd9415e5408
ori-git-acc <riocandido11@gmail.com>
2024-11-06 11:24:39 +0800

    Navbar user menu bg

--- components/Navbar.js
+++ components/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
 											leaveFrom="transform opacity-100 scale-100"
 											leaveTo="transform opacity-0 scale-95"
 										>
-											<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-900 py-1 shadow-lg ring-2 ring-white ring-opacity-10 focus:outline-none">
+											<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-800 py-1 shadow-lg ring-1 ring-white ring-opacity-10 focus:outline-none">
 												<Menu.Item>
 													{({ active }) => (
 														<a
