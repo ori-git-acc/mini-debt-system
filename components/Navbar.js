7b16b0aed52bca88b2cdb655179da0c0f692a8d8
ori-git-acc <riocandido11@gmail.com>
2024-11-06 11:21:49 +0800

    navbar hover colour on links

--- components/Navbar.js
+++ components/Navbar.js
@@ -80,7 +80,7 @@ const Navbar = () => {
 													className={classNames(
 														router.pathname === item.href
 															? "bg-gray-800 text-white"
-															: "text-gray-300 hover:bg-gray-700 hover:text-white",
+															: "text-gray-300 hover:bg-gray-800 hover:text-white",
 														"rounded-md px-3 py-2 text-sm font-medium cursor-pointer"
 													)}
 													aria-current={router.pathname === item.href ? "page" : undefined}
