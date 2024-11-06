4f4beccfaf0ec3fd2d8eb2f829d3e9d364a80573
ori-git-acc <riocandido11@gmail.com>
2024-11-06 11:29:14 +0800

    Home View debt list button bg change

--- components/HomePage.js
+++ components/HomePage.js
@@ -67,7 +67,7 @@ const HomePage = () => {
 						Add Debt
 					</button>
 					<button
-						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-gray-700 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+						className="rounded-full border border-solid bg-gray-800 border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-gray-700 hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
 						onClick={handleViewDebtListClick}
 					>
 						View Debt List
